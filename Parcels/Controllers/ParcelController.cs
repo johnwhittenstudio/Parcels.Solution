03da23fd9a2dd50893773782c8cfddb61e15c593
John Whitten <johnwhitten.studio@gmail.com>
2022-02-16 20:27:49 -0800

    add everything

--- Parcels/Controllers/ParcelController.cs
+++ Parcels/Controllers/ParcelController.cs
@@ -15,7 +15,7 @@ namespace Parcels.Controllers
     }
 
     [HttpGet("/parcel/new")]
-    public ActionResult CreateForm()
+    public ActionResult New()
     {
       return View();
     }
@@ -26,6 +26,20 @@ namespace Parcels.Controllers
       int[] dimensions = {length,height,width};
       Parcel myParcel = new Parcel(weight,dimensions,distance);
       return RedirectToAction("Index");
+    }
+
+    [HttpPost("/parcel/delete")]
+    public ActionResult DeleteAll()
+    {
+      Parcel.ClearAll();
+      return View();
+    }
+
+    [HttpGet("/parcel/{id}")]
+    public ActionResult Show(int id)
+    {
+      Parcel foundItem = Parcel.Find(id);
+      return View(foundItem);
     }
 
   }
--- Parcels/Models/Parcel.cs
+++ Parcels/Models/Parcel.cs
@@ -8,6 +8,7 @@ namespace Parcels.Models
     public int Distance { get; set; }
     public int[] Dimensions { get; set; }
     public int Volume { get; set; }
+    public int Id { get; }
     private static List<Parcel> _instances = new List<Parcel> {};
 
     public Parcel(double weight, int[] dimensions, int distance)
@@ -17,13 +18,13 @@ namespace Parcels.Models
       Dimensions = dimensions;
       Volume = Dimensions[0]*Dimensions[1]*Dimensions[2];
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public double CostToShip()
     {
       return (Weight * Volume * Distance) / 100000;
     }
-
 
 
     public static List<Parcel> GetAll()
@@ -35,5 +36,11 @@ namespace Parcels.Models
     {
       _instances.Clear();
     }
+
+    public static Parcel Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
   }
 }
